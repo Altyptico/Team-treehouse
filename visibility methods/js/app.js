d074ba1338f1b5193442f7254a2bb902135c683f
unknown <nicole@szopinski.biz>
2019-09-03 12:44:46 -0400

    add functionality to preview button and flash message on input

--- visibility methods/js/app.js
+++ visibility methods/js/app.js
@@ -1,11 +1,19 @@
-$('#flashMessage')
-  .hide()
+
+
+// const title = "My First Blog Post";
+// const content = "This is my <strong>first</strong> post!";
+
+
+$('#flashMessage').hide()
+$('#previewButton').click(function(){
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+
+  $("#blogTitlePreview").text(title);
+  $("#blogContentPreview").html(content);
+
+  $('#flashMessage')
   .slideDown(1000)
   .delay(3000)
   .slideUp();
-
-  const title = "My First Blog Post";
-  const content = "This is my <strong>first</strong> post!";
-
-  $('#blogTitlePreview').text(title);
-  $('#blogContentPreview').html(content);
+});
