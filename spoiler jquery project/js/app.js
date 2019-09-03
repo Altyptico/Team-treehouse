f6454a9a0c4dd8bc64d58182312a19b5cf6636b5
unknown <nicole@szopinski.biz>
2019-09-03 19:05:33 -0400

    add event listeners to specific target for spoiler buttons

--- spoiler jquery project/js/app.js
+++ spoiler jquery project/js/app.js
@@ -1,13 +1,21 @@
+$('.spoiler').on('click', 'button', function(event){
+    console.log(event.target);
+
+    //show the spoiler text
+    $(this).prev().show();
+//hide the "reveal spoiler" button
+    $(this).hide();
+});
+//Create the "Reveal Spoiler" Button
+const $button = $('<button>Reveal Spoiler</button>');
+//Append to web page
+$('.spoiler').append($button);
+
 //hide the spoiler text on page load
 $('.spoiler span').hide();
-//when the button is pressed
-$('.spoiler button').click(function(){
-    //show the spoiler text
-    $('.spoiler span').show();
-//hide the "reveal spoiler" button
-    $('.spoiler button').hide();
-});
 
 
 
 
+
+ 
