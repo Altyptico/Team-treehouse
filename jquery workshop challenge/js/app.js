e3b6a4d00897a337b2f6258036349bf59dceba66
unknown <nicole@szopinski.biz>
2019-09-03 14:27:44 -0400

    used descendant selectors to change text

--- jquery workshop challenge/js/app.js
+++ jquery workshop challenge/js/app.js
@@ -1,11 +1,11 @@
 // 1. Show the hidden restaurant review, or use an animation method to reveal it. 
-$('.review').show();
+$('.review').fadeIn(2000);
 // 2. Set the title of the hidden restaurant review to a different name -- whatever you'd like.  
-
+$('.review h3').text('Bayou Burger');
 // 3. Change the text of the hidden restaurant review. The review should contain HTML, such as a strong tag or a span element. 
-
+$('.review p').html('The <strong>very best</strong> gator burger Florida has to offer!');
 // 4. Change the name of the app. Change *only* the text inside the span element within the h1. 
-
+$('h1 span').text('Yumlicious!');
 // 5. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that 
 $('#flashMessage').hide().slideDown(1000).delay(2000).slideUp(1000);
   // slides the message down for 1 second
